07ac0de478b84d42295e0615c8d1cd5a6ef32234
alskra <krasnoyurchenko.a.s@gmail.com>
2024-07-16 02:45:07 +0300

    update

--- src/js/alpine/store.js
+++ src/js/alpine/store.js
@@ -5,11 +5,11 @@ Alpine.store('minLg', window.innerWidth >= 1025);
 window.addEventListener('resize', () => Alpine.store('minLg', window.innerWidth >= 1025));
 
 let scrollYCurrent = window.scrollY;
-let scrollYTimeout;
+// let scrollYTimeout;
 
 const scrollYDelta = () => {
-  clearTimeout(scrollYTimeout);
-  scrollYTimeout = setTimeout(() => Alpine.store('scrollYDelta', 0));
+  // clearTimeout(scrollYTimeout);
+  // scrollYTimeout = setTimeout(() => Alpine.store('scrollYDelta', 0));
 
   const delta = window.scrollY - scrollYCurrent;
 
