e5aee9db2d1192190884f1033b5a4d3630cbd055
alskra <krasnoyurchenko.a.s@gmail.com>
2024-07-17 01:20:32 +0300

    update

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -4,3 +4,4 @@ import '@components/header/header';
 import '@components/footer/footer';
 import '@components/aside/aside';
 import '@components/user/user';
+import '@components/target-groups/target-groups';
