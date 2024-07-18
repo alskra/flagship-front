5ab8c7319d5df5bf24300e7fb0744d365f9fa9cf
alskra <krasnoyurchenko.a.s@gmail.com>
2024-07-18 16:23:18 +0300

    update

---
+++ src/components/form-settings/form-settings.js
@@ -0,0 +1,9 @@
+import Alpine from 'alpinejs';
+import './form-settings.scss';
+
+Alpine.data('formSettings', () => ({
+  init() {
+    // eslint-disable-next-line no-console
+    console.log('form-settings init');
+  },
+}));
--- src/pages/event-settings-datetime/event-settings-datetime.js
+++ src/pages/event-settings-datetime/event-settings-datetime.js
@@ -1,3 +1,1 @@
-import '@pages/index/index';
-
-import '@components/event-info/event-info';
+import '@pages/event-settings/event-settings';
