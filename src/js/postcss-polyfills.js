bcaee37df4babefff6f8cee232cfbbb7ae0bf47f
alskra <krasnoyurchenko.a.s@gmail.com>
2024-07-18 17:26:15 +0300

    update

--- src/js/postcss-polyfills.js
+++ src/js/postcss-polyfills.js
@@ -1,11 +1,11 @@
 import cssBlankPseudo from 'css-blank-pseudo/browser';
-import 'focus-visible';
-import focusWithin from 'focus-within';
-import cssHasPseudo from 'css-has-pseudo/browser';
+// import 'focus-visible';
+// import focusWithin from 'focus-within';
+// import cssHasPseudo from 'css-has-pseudo/browser';
 
 cssBlankPseudo(document);
-focusWithin(document);
-cssHasPseudo(document);
+// focusWithin(document);
+// cssHasPseudo(document);
 
 // Fix for `css-has-pseudo`
 // eslint-disable-next-line no-extend-native
