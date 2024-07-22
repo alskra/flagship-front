74758a2f9a345f6de0c22374271fec4b089a51f5
alskra <krasnoyurchenko.a.s@gmail.com>
2024-07-22 09:01:09 +0300

    update

--- src/components/timer/timer.js
+++ src/components/timer/timer.js
@@ -1,14 +1,14 @@
 import Alpine from 'alpinejs';
 
-Alpine.data('timer', (endTime = Date.now()) => ({
+Alpine.data('timer', (endTime = 0) => ({
   endTime,
   get time() {
-    const d = Math.floor(this.endTime / 1000) - this.$store.time;
+    const d = this.endTime - this.$store.time;
 
     return d > 0 ? d : 0;
   },
   get formatTime() {
-    return this.$store.formatTime(this.time);
+    return this.$store.formatTime(this.time, false, true);
   },
   init() {
     // eslint-disable-next-line no-console
--- src/js/alpine/store.js
+++ src/js/alpine/store.js
@@ -17,40 +17,55 @@ Alpine.store('hash', window.location.hash);
 window.addEventListener('hashchange', () => Alpine.store('hash', window.location.hash));
 
 (function updateTime() {
-  Alpine.store('time', Math.floor(Date.now() / 1000));
+  Alpine.store('time', Math.floor(Date.now() / 100) * 100);
 
   requestAnimationFrame(updateTime);
 }());
 
 // Datetime
-const MINUTE = 60;
+const SECOND = 1000;
+const MINUTE = 60 * SECOND;
 const HOUR = 60 * MINUTE;
 const DAY = 24 * HOUR;
 const WEEK = 7 * DAY;
 const MONTH = 30 * DAY;
 const YEAR = 365 * DAY;
 
-Alpine.store('formatTime', (time, full = false) => {
-  const years = full && Math.floor(time / YEAR);
+Alpine.store('formatTime', (time, full = false, timer = false) => {
+  /* eslint-disable */
+  let years = full && Math.floor(time / YEAR);
   const q = Math.floor(years / 4);
   const yearsRest = (time % YEAR) - q * DAY;
 
-  const months = full && Math.floor(yearsRest / MONTH);
+  let months = full && Math.floor(yearsRest / MONTH);
   const monthsRest = yearsRest % MONTH;
 
-  const weeks = full && Math.floor(monthsRest / WEEK);
+  let weeks = full && Math.floor(monthsRest / WEEK);
   const weeksRest = monthsRest % WEEK;
 
-  const days = Math.floor((full ? weeksRest : time) / DAY);
+  let days = Math.floor((full ? weeksRest : time) / DAY);
   const daysRest = weeksRest % DAY;
 
-  const hours = Math.floor(daysRest / HOUR);
+  let hours = Math.floor(daysRest / HOUR);
   const hoursRest = daysRest % HOUR;
 
-  const minutes = Math.floor(hoursRest / MINUTE);
+  let minutes = Math.floor(hoursRest / MINUTE);
   const minutesRest = hoursRest % MINUTE;
 
-  const seconds = minutesRest;
+  let seconds = Math.floor(minutesRest / SECOND);
+  const secondsRest = minutesRest % SECOND;
+
+  const milliSeconds = secondsRest;
+
+  if (timer) {
+    // years += months > 0 ? 1 : 0;
+    // months += weeks > 0 ? 1 : 0;
+    // weeks += days > 0 ? 1 : 0;
+    // days += hours > 0 ? 1 : 0;
+    hours = minutes === 59 && seconds === 59 && milliSeconds > 0 ? (hours + 1) % 24 : hours;
+    minutes = seconds === 59 && milliSeconds > 0 ? (minutes + 1) % 60 : minutes;
+    seconds = milliSeconds > 0 ? (seconds + 1) % 60 : seconds;
+  }
 
   const str = [hours, minutes, seconds].map((number) => number.toString().padStart(2, '0')).join(':');
 
