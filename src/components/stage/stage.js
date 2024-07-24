931d9511a964afa0f40604a7b8e235d0480c3511
alskra <krasnoyurchenko.a.s@gmail.com>
2024-07-24 17:56:17 +0300

    update

--- src/components/stage/stage.js
+++ src/components/stage/stage.js
@@ -1,2 +1,15 @@
+import Alpine from 'alpinejs';
 import '@components/stage-item/stage-item';
 import './stage.scss';
+
+Alpine.data('stage', () => ({
+  isLoading: false,
+  btnLoad: {
+    ':class'() {
+      return { 'is-loading': this.isLoading };
+    },
+    ':disabled'() {
+      return this.isLoading;
+    },
+  },
+}));
